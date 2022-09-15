000d300f7479e4462790a70053f6a2f88e825ce0
hp-pc <navyashree811@gmail.com>
2022-09-15 23:00:00 +0530

    Added : Retrieve all employee who joined in particular date range

--- src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
@@ -1,5 +1,4 @@
-/*UC4:- Ability to update the salary i.e. the base pay for 
- *    Employee Terisa to 3000000 Database using JDBC PreparedStatement. 
+/* * UC5:- Ability to retrieve all employees who have joined in a particular data range from the payroll service database .
  *
  * @author : Navaya Shree
  * @since : 9/15/2022
@@ -10,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.Date;
 
 public class EmployeePayroll {
 	static Connection con;
@@ -19,7 +19,7 @@ public class EmployeePayroll {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
-		String qry = "update employee_payroll.payroll_service set basic_pay=3000000.00 where name='Terisa'";
+		String qry = "select * from employee_payroll where start between Cast('2020-01-01' as date) and date (now())";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
@@ -31,6 +31,27 @@ public class EmployeePayroll {
 
 			pstmt = con.prepareStatement(qry);
 			System.out.println("Data Update");
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				long phone_number = rs.getLong(3);
+				String address = rs.getString(4);
+				String gender = rs.getString(5);
+				double basic_pay = rs.getDouble(6);
+				double deductions = rs.getDouble(7);
+				double taxable_pay = rs.getDouble(8);
+				double tax = rs.getDouble(9);
+				double met_pay = rs.getDouble(10);
+				Date state = rs.getDate(11);
+
+				System.out.println("EmpId :-" + id + "  " + "EmpName :- " + name + " " + "Emp_PhoneNo. :-"
+						+ phone_number + " " + "EmpAddress :- " + address + " " + "Gender :- " + gender + " "
+						+ "Basic_Pay " + basic_pay + " " + " Deduction :- " + deductions + " " + "Taxable_pay :- "
+						+ taxable_pay + " " + "Tax :-" + tax + " " + "Met_Pay :- " + met_pay + " " + "Date :-" + state);
+			}
 
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
