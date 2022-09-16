cb4933d184f2e5857d9c5a189ea9bde5fd0377c3
hp-pc <navyashree811@gmail.com>
2022-09-16 17:09:43 +0530

    Added : Refactor code create preparedStatement to Retrieve payroll data and by Name

--- src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
@@ -1,24 +1,27 @@
-/* UC6:- Ability to find sum, average, min, max and number of male and female employees .
+/* UC4:- -Refactor Code to do the following Create the preparedStatement to Retrieve payroll data and By Name .
  *
  * @author : Navaya Shree
- * @since : 9/15/2022
+ * @since : 16/9/2022
  */
 package com.bridgelabz.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 
 public class EmployeePayroll {
 	static Connection con;
-	static Statement stmt;
-	static ResultSet rs;
 
 	public static void main(String[] args) throws Exception, SQLException {
 
-		String qry = "select avg(basic_pay), sum(basic_pay), min(basic_pay), max(basic_pay) from employee_payroll where gender = 'F' group by gender; ";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String qry = "select * from employee_payroll where EName='Terisa'";
 		try {
+			Class.forName("com.mysql.jdbc.Driver");
 
 			System.out.println("Driver Class Loaded");
 
@@ -26,14 +29,30 @@ public class EmployeePayroll {
 
 			System.out.println("Connetion Establish with db server");
 
-			stmt = con.createStatement();
+			pstmt = con.prepareStatement(qry);
 			System.out.println("Data Update");
 
-			rs = stmt.executeQuery(qry);
+			rs = pstmt.executeQuery();
 
 			while (rs.next()) {
-				double sum = rs.getDouble(1);
-				System.out.println("avgrage of basic_pay : " + sum);
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				long phone_number = rs.getLong(3);
+				String address = rs.getString(4);
+				String department = rs.getString(5);
+				String gender = rs.getString(6);
+				double basic_pay = rs.getDouble(7);
+				double deductions = rs.getDouble(8);
+				double taxable_pay = rs.getDouble(9);
+				double tax = rs.getDouble(10);
+				double met_pay = rs.getDouble(11);
+				Date state = rs.getDate(12);
+
+				System.out.println("EmpId :-" + id + "  " + "EmpName :- " + name + " " + "Emp_PhoneNo. :-"
+						+ phone_number + " " + "EmpAddress :- " + address + " " + " EmpDepartment :-" + department + " "
+						+ "Gender :- " + gender + " " + "Basic_Pay " + basic_pay + " " + " Deduction :- " + deductions
+						+ " " + "Taxable_pay :- " + taxable_pay + " " + "Tax :-" + tax + " " + "Met_Pay :- " + met_pay
+						+ " " + "Date :-" + state);
 			}
 
 		} catch (ClassNotFoundException | SQLException e) {
@@ -49,9 +68,9 @@ public class EmployeePayroll {
 				}
 
 			}
-			if (stmt != null) {
+			if (pstmt != null) {
 				try {
-					stmt.close();
+					pstmt.close();
 
 				} catch (SQLException e) {
 					e.printStackTrace();
@@ -68,5 +87,6 @@ public class EmployeePayroll {
 			}
 			System.out.println("Closed All Resources");
 		}
+
 	}
 }
